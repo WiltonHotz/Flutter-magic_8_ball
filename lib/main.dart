d3a3f6e2023f3bd4bcd8742014745977d94a3a6d
WiltonHotz <v.horwing@gmail.com>
2020-03-19 12:11:40 +0100

    magic 8 ball project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.indigo.shade700,
+            title: Center(
+              child: Text(
+                'ASK ME ANYTHING',
+                style: TextStyle(
+                  letterSpacing: 2.0,
+                ),
+              ),
+            ),
+          ),
+          body: MagicPage(),
+          backgroundColor: Colors.blue,
+        ),
       ),
     );
+
+class MagicPage extends StatefulWidget {
+  @override
+  _MagicPageState createState() => _MagicPageState();
+}
+
+class _MagicPageState extends State<MagicPage> {
+  int currentImage = 1;
+
+  void setAnswer() {
+    currentImage = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                setAnswer();
+              });
+            },
+            child: Image.asset('images/ball$currentImage.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
